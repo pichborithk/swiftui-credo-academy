868c90be649b2360632fc6c30de4a87895629276
Pichborith Kong <pichborith@gmail.com>
2024-09-25 21:33:11 -0700

    initialize ui demo

---
+++ 0- UI Demo/src/ui_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_demoApp.swift
+//  ui-demo
+//
+//  Created by Pichborith Kong on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ui_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
