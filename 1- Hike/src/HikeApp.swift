33cad46ff7b4f5ae437301ff3077525a691ed624
Pichborith Kong <pichborith@gmail.com>
2024-10-03 10:42:13 -0700

    initialize Hike app

---
+++ 1- Hike/src/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Pichborith Kong on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
