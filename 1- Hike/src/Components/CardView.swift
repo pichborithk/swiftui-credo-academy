488947fe2632f62539933cce86d73150569b8717
Pichborith Kong <pichborith@gmail.com>
2024-10-03 21:34:37 -0700

    add custom circle to card view

--- 1- Hike/src/Components/CardView.swift
+++ 1- Hike/src/Components/CardView.swift
@@ -66,12 +66,7 @@ struct CardView: View {
                 // MARK: - MAIN CONTENT
 
                 ZStack {
-                    Circle()
-                        .fill(LinearGradient(
-                            colors: [.colorIndigoMedium, .colorSalmonLight],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing))
-                        .frame(width: 256, height: 256)
+                    CustomCircleView()
 
                     Image("image-\(imageNumber)")
                         .resizable()
