56b111987d3bc253cfffaff699e82b06de7172fb
Pichborith Kong <pichborith@gmail.com>
2024-10-03 21:18:39 -0700

    add example for color extension to use with old version

---
+++ 1- Hike/src/Utilities/ColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Pichborith Kong on 10/3/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+}
