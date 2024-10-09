702c6605cd91ef801ef7f1f0d124c9f9e318aa28
Pichborith Kong <pichborith@gmail.com>
2024-10-08 19:49:49 -0700

    initialize restart project

---
+++ 2- Restart/src/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Pichborith Kong on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
