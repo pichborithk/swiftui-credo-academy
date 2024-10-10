5c40c3fe63be297647dc0ac702e355ccba0cb7fa
Pichborith Kong <pichborith@gmail.com>
2024-10-10 14:47:51 -0700

    build onboarding screen

--- 2- Restart/src/Screens/OnboardingView.swift
+++ 2- Restart/src/Screens/OnboardingView.swift
@@ -13,21 +13,113 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTIES
-    
+
     @AppStorage("onboarding") var isOnboardingViewAction: Bool = true // This is not declare, it set value to "true" in case "onboarding" key doesn't exist
-    
+
     // MARK: BODY
-    
+
     var body: some View {
-        VStack {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button {
-                isOnboardingViewAction = false
-            } label: {
-                Text("Start")
-            }
-        } //: VSTACK
+        ZStack {
+            Color(.customBlue)
+                .ignoresSafeArea(.all, edges: .all)
+
+            VStack(spacing: 20) {
+                Spacer()
+
+                // MARK: - HEADER
+
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+
+                    Text("""
+                    It's not how much we given but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+
+                // MARK: - CENTER
+
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+
+                Spacer()
+
+                // MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(.customRed)
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(.customRed)
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewAction = false
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
