9f0c5d944392a5b13750f4ac804ee4245c7df211
Pichborith Kong <pichborith@gmail.com>
2024-10-11 16:59:04 -0700

    replace circle group view

--- 2- Restart/src/Screens/OnboardingView.swift
+++ 2- Restart/src/Screens/OnboardingView.swift
@@ -48,14 +48,7 @@ struct OnboardingView: View {
                 // MARK: - CENTER
 
                 ZStack {
-                    ZStack {
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 40)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width: 260, height: 260, alignment: .center)
-                    } //: ZSTACK
+                    CircleGroupView() //: ZSTACK
                     
                     Image("character-1")
                         .resizable()
@@ -123,3 +116,5 @@ struct OnboardingView: View {
         } //: ZSTACK
     }
 }
+
+
