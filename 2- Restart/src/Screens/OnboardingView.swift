31185132f36784e73d58af63f6b9310b965ed475
Pichborith Kong <pichborith@gmail.com>
2024-10-13 15:54:03 -0700

    make swipe button

--- 2- Restart/src/Screens/OnboardingView.swift
+++ 2- Restart/src/Screens/OnboardingView.swift
@@ -14,7 +14,11 @@ import SwiftUI
 struct OnboardingView: View {
     // MARK: - PROPERTIES
 
-    @AppStorage("onboarding") var isOnboardingViewAction: Bool = true // This is not declare, it set value to "true" in case "onboarding" key doesn't exist
+    // This is not declare, it set value to "true" in case "onboarding" key doesn't exist
+    @AppStorage("onboarding") var isOnboardingViewAction: Bool = true
+
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
 
     // MARK: BODY
 
@@ -49,7 +53,7 @@ struct OnboardingView: View {
 
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
-                    
+
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
@@ -58,37 +62,37 @@ struct OnboardingView: View {
                 Spacer()
 
                 // MARK: - FOOTER
-                
+
                 ZStack {
                     // PARTS OF THE CUSTOM BUTTON
-                    
+
                     // 1. BACKGROUND (STATIC)
-                    
+
                     Capsule()
                         .fill(.white.opacity(0.2))
-                    
+
                     Capsule()
                         .fill(.white.opacity(0.2))
                         .padding(8)
                     // 2. CALL-TO-ACTION (STATIC)
-                    
+
                     Text("Get Started")
                         .font(.system(.title3, design: .rounded))
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .offset(x: 20)
-                    
+
                     // 3. CAPSULE (DYNAMIC WIDTH)
-                    
+
                     HStack {
                         Capsule()
                             .fill(.customRed)
-                            .frame(width: 80)
-                        
+                            .frame(width: buttonOffset + 80)
+
                         Spacer()
                     }
                     // 4. CIRCLE (DRAGGABLE)
-                    
+
                     HStack {
                         ZStack {
                             Circle()
@@ -96,25 +100,38 @@ struct OnboardingView: View {
                             Circle()
                                 .fill(.black.opacity(0.15))
                                 .padding(8)
-                            
+
                             Image(systemName: "chevron.right.2")
                                 .font(.system(size: 24, weight: .bold))
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewAction = false
-                        }
-                        
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        // First Condition: It will only run when the dragging has been started in the right direction (from left to right direction)
+                                        // Second Condition: It prevent button to go outside the Zstack
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset >= buttonWidth * 0.60 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewAction = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        ) //: GESTURE
+
                         Spacer()
                     }
-                    
                 } //: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
     }
 }
-
-
