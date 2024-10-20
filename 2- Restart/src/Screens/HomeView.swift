b13a09a1320d77d0ce72bdd34c1957c09664b385
Pichborith Kong <pichborith@gmail.com>
2024-10-20 14:07:25 -0700

    add animation to home view screen

--- 2- Restart/src/Screens/HomeView.swift
+++ 2- Restart/src/Screens/HomeView.swift
@@ -13,41 +13,49 @@ import SwiftUI
 
 struct HomeView: View {
     // MARK: - PROPERTIES
-    
+
     @AppStorage("onboarding") var isOnboardingViewAction: Bool = false // This is not declare, it set value to "true" in case "onboarding" key doesn't exist
-    
+
+    @State private var isAnimating: Bool = false
+
     // MARK: BODY
+
     var body: some View {
         VStack(spacing: 20) {
             Spacer()
+
             // MARK: - HEADER
+
             ZStack {
-                CircleGroupView(shapeColor: Color(.gray), shapeOpacity: 0.1)
-                
+                CircleGroupView(shapeColor: Color(.gray), shapeOpacity: 0.1, isAnimating: $isAnimating)
+
                 Image("character-2")
                     .resizable()
                     .scaledToFit()
                     .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(.easeInOut(duration: 4).delay(3.0).repeatForever(), value: isAnimating)
             }
-            
+
             // MARK: - CENTER
+
             Text("The time that leads to mastery is dependent on the intensity of our focus.")
                 .font(.title3)
                 .fontWeight(.light)
                 .foregroundColor(.secondary)
                 .multilineTextAlignment(.center)
                 .padding()
-            
+
             Spacer()
+
             // MARK: - FOOTER
-            
-            
+
             Button {
                 isOnboardingViewAction = true
             } label: {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
-                
+
                 Text("Restart")
                     .font(.system(.title2, design: .rounded))
                     .fontWeight(.bold)
@@ -56,5 +64,11 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         } //: VSTACK
+        .onAppear {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                isAnimating = true
+//            }
+            isAnimating = true
+        }
     }
 }
